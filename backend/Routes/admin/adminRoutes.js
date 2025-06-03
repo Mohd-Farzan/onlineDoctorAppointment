904ddba28dc03bee6d44f5dbf58a226d847831f7
Mohd Farzan <mohdfarzan701@gmail.com>
2025-06-04 02:14:18 +0300

    fix routing

--- backend/Routes/admin/adminRoutes.js
+++ backend/Routes/admin/adminRoutes.js
@@ -1,8 +1,6 @@
 const express =require('express');
 const { createDoctor } = require('../../Controlller/doctor/doctorcontrollers');
 const route=express.Router();
-route.get('/dashboard');
 route.post('/doctor',createDoctor)
-route.get('/doctor-data',)
 route.post("/create-doctor",createDoctor);
 module.exports=route
