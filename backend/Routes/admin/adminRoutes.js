66234cc1abef6307c0ad2d48d7c303f10d454f25
Mohd Farzan <mohdfarzan701@gmail.com>
2025-06-04 01:38:39 +0300

    installing express done

--- backend/Routes/admin/adminRoutes.js
+++ backend/Routes/admin/adminRoutes.js
@@ -2,7 +2,7 @@ const express =require('express');
 const { createDoctor } = require('../../Controlller/doctor/doctorcontrollers');
 const route=express.Router();
 route.get('/dashboard');
-route.post('doctor',createDoctor)
+route.post('/doctor',createDoctor)
 route.get('/doctor-data',)
 route.post("/create-doctor",createDoctor);
 module.exports=route
